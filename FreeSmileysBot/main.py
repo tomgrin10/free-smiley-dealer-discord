48943a63dce4932fc904fc48b9d9f6859bf34fca
Unknown <tomgrin10@gmail.com>
2018-08-08 20:05:17 +0300

    Added help when mentioned or prefixed

--- FreeSmileysBot/main.py
+++ FreeSmileysBot/main.py
@@ -25,6 +25,10 @@ async def on_ready():
 
 @bot.event
 async def on_message(message: discord.Message):
+    # Send help
+    if message.content.startswith(bot.command_prefix) or message.content.startswith(f"<@{bot.user.id}>"):
+        await send_help(message.channel)
+
     # Iterate every record
     for record in smileys_data["smileys"]:
         # Iterate every paid smiley
@@ -34,12 +38,16 @@ async def on_message(message: discord.Message):
                 print(message.content + " detected.")
                 # Create the embed
                 title = random.choice(smileys_data["titles"])
-                embed = discord.Embed(title=title, description="<@{}>".format(message.author.id))
+                embed = discord.Embed(title=title, description=f"<@{message.author.id}>")
                 free_smiley_url = random.choice(record["free_smileys"])
                 embed.set_image(url=free_smiley_url)
                 await bot.send_message(message.channel, embed=embed)
 
                 return
+
+
+async def send_help(channel: discord.Channel):
+    await bot.send_message(channel, smileys_data["help"])
 
 
 bot.run(config["token"])
