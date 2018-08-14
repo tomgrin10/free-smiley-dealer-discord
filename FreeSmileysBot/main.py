d800f3610026dc201a3cb064b4aebbcbb07a7c9b
Unknown <tomgrin10@gmail.com>
2018-08-15 02:10:12 +0300

    Changed help msg, fixed bug etc

--- FreeSmileysBot/main.py
+++ FreeSmileysBot/main.py
@@ -13,6 +13,8 @@ STATIC_DATA_FILENAME = "static_data.json"
 DISCORD_EMOJI_CODES_FILENAME = "discord_emoji_codes.json"
 DYNAMIC_DATA_FILENAME = "dynamic_data.json"
 DEFAULT_SMILEY_SIZE = 150
+MIN_SMILEY_SIZE = 40
+MAX_SMILEY_SIZE = 300
 
 # Load data
 with open(TOKEN_FILENAME) as f:
@@ -22,7 +24,7 @@ with open(STATIC_DATA_FILENAME, 'r') as f:
 with open(DISCORD_EMOJI_CODES_FILENAME, 'r') as f:
     DISCORD_CODE_TO_EMOJI = json.load(f)
     DISCORD_EMOJI_TO_CODE = {v: k for k, v in DISCORD_CODE_TO_EMOJI.items()}
-if pathlib.Path("/path/to/file").is_file():
+if pathlib.Path(DYNAMIC_DATA_FILENAME).is_file():
     with open(DYNAMIC_DATA_FILENAME, 'r') as f:
         DYNAMIC_DATA = json.load(f)
 else:
@@ -95,13 +97,7 @@ async def on_message(message: discord.Message):
 async def command_help(ctx: commands.Context):
     print("help")
     # "help": "\n",
-    await bot.say(f"""
-<@{ctx.message.author.id}>
-If you use regular (paid) smileys in your message, I will correct you.
-Try it out, type `:joy:`.
-You can also use the command `s!s` with the paid smiley name, example `s!s joy`.
-Support server here: https://discord.gg/2XCVVx
-    """)
+    await bot.say(f"{STATIC_DATA['help']}")
 
 
 @bot.command(pass_context=True, name="smiley", aliases=["s"])
@@ -126,18 +122,26 @@ async def command_smiley(ctx: commands.Context, emoji_name_message: str):
 
 @bot.command(pass_context=True, name="size", aliases=["height"])
 async def command_size(ctx: commands.Context, size: str):
+    # Check for permissions
     if not ctx.message.author.server_permissions.manage_channels:
         await bot.say(":x: **This command requires you to have `Manage Channels` permission to use it.**")
         return
 
+    # Validate size parameter
     try:
         size = int(size)
     except ValueError:
         await bot.say("Invalid size.")
         return
 
+    if size < MIN_SMILEY_SIZE or size > MAX_SMILEY_SIZE:
+        await bot.say(f"Size must be between {MIN_SMILEY_SIZE} and {MAX_SMILEY_SIZE}.")
+        return
+
+    # Lock thread
     async with DYNAMIC_DATA_LOCK:
         with open(DYNAMIC_DATA_FILENAME, 'w') as f:
+            # Change size and save in file
             DYNAMIC_DATA["sizes"][ctx.message.server.id] = size
             json.dump(DYNAMIC_DATA, f, indent=4)
 
