c795bbf37ee58076b61f94a8ceb26eea0cdba650
unknown <tomgrin10@gmail.com>
2018-10-15 03:39:54 +0300

    Added new event and smileys

--- FreeSmileysBot/main.py
+++ FreeSmileysBot/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from typing import Union
 import asyncio
 import json
 import random
@@ -11,6 +12,7 @@ import requests
 import furl
 
 # Constants
+MESSAGE_CHARACTER_LIMIT = 2000
 CONFIG_FILENAME = "config.json"
 STATIC_DATA_FILENAME = "static_data.json"
 DISCORD_EMOJI_CODES_FILENAME = "discord_emoji_codes.json"
@@ -52,41 +54,72 @@ def emoji_lis(s: str) -> list:
             yield c
 
 
+def split_message_for_discord(content: str, divider: str = None):
+    if divider is None:
+        segments = content
+    else:
+        segments = [s + divider for s in content.split(divider)]
+
+    messages = [""]
+    for segment in segments:
+        if len(messages[len(messages) - 1]) + len(segment) >= MESSAGE_CHARACTER_LIMIT:
+            messages.append(segment)
+        else:
+            messages[len(messages) - 1] += segment
+
+    return messages
+
+
 def log(s: str):
     async def async_log(s: str):
         print(s)
-        await bot.send_message(bot.get_channel(CONFIG["logs_channel"]), s)
+        for segment in split_message_for_discord(s):
+            await bot.get_channel(CONFIG["logs_channel"]).send(segment)
         
-    asyncio.get_event_loop().create_task(async_log(s))
+    asyncio.create_task(async_log(s))
 
 
-def add_surprise_to_url(url: str):
+def rand_color_image_url(url: str) -> str:
     return furl.furl(url).add({"hue": random.randint(-100, 100),
                                "saturation": random.randint(-100, 100),
                                "lightness": random.randint(-100, 100)}).url
 
 
-def get_free_smiley_url(emoji_name: str, message: discord.Message, smiley_num: int = None, allow_surprise=False):
+def spookify_image_url(url: str, spook_value: int) -> str:
+    max_values = {"contrast": 36, "shadows": random.choice([100, -65]), "saturation": -100, "lightness": -100}
+    return furl.furl(url).add({k: round(v * spook_value) for k, v in max_values.items()})
+
+
+def get_free_smiley_url(emoji_name: str, message: discord.Message, smiley_num: str = None, allow_surprise=False) -> Union[str, None]:
     # Iterate emojis in message
     for p_smileys in STATIC_DATA["smileys"]:
         if emoji_name not in p_smileys:
             continue
 
-        print(message.content + " detected.")
-
         # Get and format url
-        dir_url = f"{STATIC_DATA['base_url']}/{p_smileys[0]}"
+        dir_name = p_smileys[0]
+        dir_url = f"{STATIC_DATA['base_url']}/{dir_name}"
         dir_json = json.loads(requests.get(f"{dir_url}?json=true").text)
 
         if smiley_num is None:
             # Get random smiley
             try:
-                file_name = random.choice(dir_json['files'])['name']
+                # If ralf than maybe golden
+                if dir_name == "ralf":
+                    if random.random() * 100 < STATIC_DATA["golden_ralf_chance"]:
+                        file_name = "golden.png"
+                        log(f"Golden ralf has appeared in `{message.guild}`")
+                    else:
+                        file_name = "regular.png"
+                # If regular smiley
+                else:
+                    file_name = random.choice(dir_json['files'])['name']
             except IndexError:
                 return
         else:
             # Get smiley from smiley number
-            matches = list(filter(lambda file: str(smiley_num) in file["name"], dir_json["files"]))
+            print(dir_json["files"])
+            matches = list(filter(lambda file: smiley_num in file["name"], dir_json["files"]))
             if len(matches) == 0:
                 return
             file_name = matches[0]["name"]
@@ -94,15 +127,32 @@ def get_free_smiley_url(emoji_name: str, message: discord.Message, smiley_num: i
         free_smiley_url = f"{dir_url}/{urllib.parse.quote(file_name)}"
 
         # Attach height parameter to url
-        if message.server.id in DYNAMIC_DATA['sizes']:
-            free_smiley_url += f"?scale.height={DYNAMIC_DATA['sizes'][message.server.id]}"
+        if str(message.guild.id) in DYNAMIC_DATA['sizes']:
+            free_smiley_url += f"?scale.height={DYNAMIC_DATA['sizes'][str(message.guild.id)]}"
         else:
             free_smiley_url += f"?scale.height={DEFAULT_SMILEY_SIZE}"
-           
-        # Random surprise
-        if not file_name.endswith(".gif") and random.random() * 100 < STATIC_DATA["surprise_chance"]:
-            free_smiley_url = add_surprise_to_url(free_smiley_url)
-            log(f"Surprise in `{message.server}`")
+
+        # Random surprises
+        if allow_surprise and not file_name.endswith(".gif"):
+            # Recolor rare smiley suprise
+            if random.random() * 100 < STATIC_DATA["surprise_chance"]:
+                free_smiley_url = rand_color_image_url(free_smiley_url)
+                log(f"Surprise in `{message.guild}`")
+
+            # Halloween spooker
+            elif random.random() * 100 < 50:
+                curr_date = datetime.datetime.now()
+                start_date = datetime.datetime(curr_date.year, 10, 15, 0)
+                halloween_date = datetime.datetime(curr_date.year, 10, 31, 0)
+                end_date = datetime.datetime(curr_date.year, 11, 2, 0)
+
+                # Check if halloween time
+                if start_date < curr_date < end_date:
+                    if curr_date < halloween_date:
+                        spook_value = (curr_date - start_date) / (halloween_date - start_date)
+                    else:
+                        spook_value = 1
+                    free_smiley_url = spookify_image_url(free_smiley_url, spook_value)
 
         return free_smiley_url
 
@@ -111,7 +161,7 @@ def get_free_smiley_url(emoji_name: str, message: discord.Message, smiley_num: i
 async def on_ready():
     await bot.change_presence(game=discord.Game(name=STATIC_DATA["game"]))
     log("Bot is ready.")
-    log(f"{len(bot.servers)} servers:\n{[s.name for s in bot.servers]}")
+    log(f"{len(bot.guilds)} guilds:\n{[g.name for g in bot.guilds]}")
 
 
 @bot.event
@@ -129,22 +179,20 @@ async def on_message(message: discord.Message):
                 # Create the embed
                 embed = discord.Embed(title=random.choice(STATIC_DATA["titles"]))
                 embed.set_image(url=url)
-                await bot.send_message(message.channel, content=f"<@{message.author.id}>", embed=embed)
+                await message.channel.send(content=f"{message.author.mention}", embed=embed)
 
                 break
 
         await bot.process_commands(message)
 
     except Exception as e:
-        bot.send_message(message.channel, ":x: **An error occurred.**")
+        message.channel.send(":x: **An error occurred.**")
         log(f"Error: {e}")
 
 
-@bot.command(pass_context=True, name="help", aliases=["h"])
+@bot.command(name="help", aliases=["h"])
 async def command_help(ctx: commands.Context):
-    p = bot.command_prefix
-    print(str(p))
-    await bot.say("""
+    await ctx.send("""
 If you use **paid smileys (emojis)** in your message, I will correct you.
 **Try it out!** Type `:joy:`
 
@@ -163,95 +211,102 @@ Sets the size of the smileys. (default:`150`) (between `40-300`)
 **Format:** `s!size <size>`
 **Example:** `s!size 200`""")
     embed.add_field(name=":red_circle: invite/inv", value="""
-Invite me to your server!""")
+Invite me to your guild!""")
     embed.add_field(name=":red_circle: support", value="""
-Come to my support server for help or suggestions!""")
+Come to my support guild for help or suggestions!""")
+    
+    await ctx.author.send(content, embed=embed)
 
-    await bot.send_message(ctx.message.author, content, embed=embed)
 
-
-@bot.command(pass_context=True, name="smiley", aliases=["s"])
+@bot.command(name="smiley", aliases=["s"])
 async def command_smiley(ctx: commands.Context, emoji_name: str, smiley_num: str = None):
-    try:
-        smiley_num = int(smiley_num)
-    except (ValueError, TypeError):
-        smiley_num = None
-
     url = get_free_smiley_url(emoji_name, ctx.message, smiley_num=smiley_num)
     if url is not None:
         print(ctx.message.content + " detected.")
-        await bot.say(url)
+        await ctx.send(url)
     else:
-        await bot.say(":x: **Smiley not found.**")
+        await ctx.send(":x: **Smiley not found.**")
 
 
-@bot.command(pass_context=True, name="size", aliases=["height"])
+@bot.command(name="size", aliases=["height"])
 async def command_size(ctx: commands.Context, size: str):
     # Check for permissions
-    if not ctx.message.author.server_permissions.manage_channels:
-        await bot.say(":x: **This command requires you to have `Manage Channels` permission to use it.**")
+    if not ctx.author.guild_permissions.manage_channels:
+        await ctx.send(":x: **This command requires you to have `Manage Channels` permission to use it.**")
         return
 
     # Validate size parameter
     try:
         size = int(size)
     except ValueError:
-        await bot.say(":x: **Invalid size.**")
+        await ctx.send(":x: **Invalid size.**")
         return
 
     if size < MIN_SMILEY_SIZE or size > MAX_SMILEY_SIZE:
-        await bot.say(f":x: **Size must be between {MIN_SMILEY_SIZE} and {MAX_SMILEY_SIZE}.**")
+        await ctx.send(f":x: **Size must be between {MIN_SMILEY_SIZE} and {MAX_SMILEY_SIZE}.**")
         return
 
     # Lock thread
     async with DYNAMIC_DATA_LOCK:
         with open(DYNAMIC_DATA_FILENAME, 'w') as f:
             # Change size and save in file
-            DYNAMIC_DATA["sizes"][ctx.message.server.id] = size
+            DYNAMIC_DATA["sizes"][str(ctx.guild.id)] = size
             json.dump(DYNAMIC_DATA, f, indent=4)
 
-    log(f"Size changed to {size} in {ctx.message.server}.")
-    await bot.say(f":white_check_mark: Size changed to {size}.")
+    log(f"Size changed to {size} in {ctx.message.guild}.")
+    await ctx.send(f":white_check_mark: Size changed to {size}.")
 
 
 @bot.command(name="invite", aliases=["inv"])
-async def command_invite():
+async def command_invite(ctx):
     if "invite" in CONFIG and CONFIG["invite"] is not None:
-        await bot.say(CONFIG["invite"])
+        await ctx.send(CONFIG["invite"])
 
 
 @bot.command(name="support")
-async def command_support():
+async def command_support(ctx):
     if "support" in CONFIG and CONFIG["support"] is not None:
-        await bot.say(CONFIG["support"])
+        await ctx.send(CONFIG["support"])
 
     
 @bot.command(name="uptime")
-async def command_uptime():
+async def command_uptime(ctx):
     delta = datetime.timedelta(seconds=time.time() - T0)
     days = delta.days
     hours, rem = divmod(delta.seconds, 3600)
     mins, secs = divmod(rem, 60)
-    await bot.say(f"{days}d {hours}h {mins}m {secs}s")
+    await ctx.send(f"{days}d {hours}h {mins}m {secs}s")
 
 
-@bot.command(name="log", pass_context=True)
+@bot.command(name="log")
 async def command_log(ctx, *, to_log):
-    if ctx.message.author.id == "190224152978915329":
+    shortcuts = {
+        "len": "len(bot.guilds)",
+        "guilds": "[g.name for g in bot.guilds]",
+    }
+
+    if to_log in shortcuts:
+        to_log = shortcuts[to_log]
+
+    if ctx.author.id == 190224152978915329:
         log(f"{eval(to_log)}")
 
 
 # Reload data every once in a while
 async def reload_data_continuously():
     global STATIC_DATA, DISCORD_CODE_TO_EMOJI, DISCORD_EMOJI_TO_CODE
+    await bot.wait_until_ready()
     while True:
-        await asyncio.sleep(60)
-        t0 = time.time()
+        sleep_task = asyncio.create_task(asyncio.sleep(60))
+        try:
+            t0 = time.time()
 
-        with open(STATIC_DATA_FILENAME, 'r') as f:
-            STATIC_DATA = json.load(f)
+            with open(STATIC_DATA_FILENAME, 'r') as f:
+                STATIC_DATA = json.load(f)
 
-        print(f"Reloaded data in {int(round((time.time() - t0) * 1000))} ms.")
+            print(f"Reloaded data in {int(round((time.time() - t0) * 1000))} ms.")
+        finally:
+            await sleep_task
 
 
 bot.loop.create_task(reload_data_continuously())
