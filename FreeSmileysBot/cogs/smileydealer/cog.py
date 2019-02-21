4586c7abdc8a266fe73735479595ddc3f8b0f2cb
Tom <tomgrin10@gmail.com>
2019-02-21 18:00:10 +0200

    fix

--- FreeSmileysBot/cogs/smileydealer/cog.py
+++ FreeSmileysBot/cogs/smileydealer/cog.py
@@ -475,7 +475,7 @@ class FreeSmileyDealerCog:
     @extensions.command(name="size", category="settings",
                         brief="Change the size of the smiley image when correcting.",
                         usage="[pixels|*default*] [**]",
-                        enable=False)
+                        enabled=False)
     @commands.guild_only()
     @commands.has_permissions(manage_channels=True)
     async def command_size(self, ctx: commands.Context,
