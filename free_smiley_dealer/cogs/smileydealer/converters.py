59db95f42a083903e33bf9e758450d8b511e176d
Tom <tomgrin10@gmail.com>
2020-06-17 12:40:57 +0300

    Add enum converter

--- free_smiley_dealer/cogs/smileydealer/converters.py
+++ free_smiley_dealer/cogs/smileydealer/converters.py
@@ -1,17 +1,19 @@
-from typing import Optional
+from enum import Enum
+from typing import Optional, TypeVar, Type
 
 import discord
 from discord.ext import commands
-
 
 __all__ = ["SettingsChannelConverter", "SettingsDefaultConverter"]
 
 
 class SettingsDefaultConverter(commands.Converter):
-    def __init__(self, default_value=None):
+    _DefaultValueType = TypeVar('_DefaultValueType')
+
+    def __init__(self, default_value: _DefaultValueType = None):
         self.default_value = default_value
 
-    async def convert(self, ctx, arg):
+    async def convert(self, ctx: commands.Context, arg: str) -> _DefaultValueType:
         if "default".startswith(arg):
             return self.default_value
         else:
@@ -30,3 +32,17 @@ class SettingsChannelConverter(commands.TextChannelConverter):
             return None
 
         return await super().convert(ctx, arg)
+
+
+class EnumConverter(commands.Converter):
+    _EnumType = TypeVar('_EnumType', bound=Enum)
+
+    def __init__(self, enum_type: Type[_EnumType]):
+        self.enum_type = enum_type
+
+    async def convert(self, ctx: commands.Context, arg: str) -> _EnumType:
+        for enum_obj in self.enum_type:
+            enum_obj: Enum
+
+            if enum_obj.name.lower().startswith(arg.lower()):
+                return enum_obj
